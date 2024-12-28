41ad8477aec4f7f5ad166d6d123ce5262d8b4cef
GOURAVSINGH19 <gouravsingh4495@example.com>
2024-12-28 15:58:59 +0530

    init-2 😶‍🌫️

--- src/app.js
+++ src/app.js
@@ -14,7 +14,13 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({
+  credentials: true,
+  origin: [
+    "http://localhost:5173",
+    "https://research-mangement-frontend.vercel.app"
+  ]
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
